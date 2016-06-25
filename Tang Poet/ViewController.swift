220a0860706a2ccb9c2e5cdc5f4670cf1c731489
Fly <imbearfly@efgpdigiwinde-macbook-air.local>
2016-06-26 00:58:54 +0800

    完成相思

--- Tang Poet/ViewController.swift
+++ Tang Poet/ViewController.swift
@@ -8,8 +8,28 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource{
+    
+    
+    @IBOutlet weak var Poetry: UITableView!
+    let poetrys = [("☞ 白居易"),("☞ 李白"),("☞ 孟浩然"),("☞ 杜甫"),("☞ 王維")]
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1;
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+       return poetrys.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        let poetry = self.poetrys[indexPath.row]
+        cell.textLabel?.text = poetry
+        return cell
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
